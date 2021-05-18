c4bfb7611e4943ad11fcb7e1ac1dc2bbe0084237
Charlotte Dawson <csdaw@outlook.com>
2021-05-18 16:39:14 +0100

    Make venn_counts() function

--- R/generate_counts.R
+++ R/generate_counts.R
@@ -1,7 +1,12 @@
 generate_counts <- function(data) {
-  l <- data$elements
+  venn_counts(data$elements)
+}
+
+venn_counts <- function(l) {
   n_sets <- length(l)
-  names(l) <- LETTERS[1:n_sets]
+
+  # give arbitrary names to sets if they are not named already
+  if(is.null(names(l))) names(l) <- LETTERS[1:n_sets]
 
   l_boolean <- lapply(l, function(i) as.numeric(unique(unlist(l)) %in% i))
 
@@ -10,7 +15,6 @@ generate_counts <- function(data) {
   n_segments <- 2^n_sets
 
   segments <- matrix(0, n_segments, n_sets)
-
   colnames(segments) <- names(l)
   for (j in 1:n_sets) segments[,j] <- rep(0:1,times=2^(j-1),each=2^(n_sets-j))
   cbind(segments, counts)
