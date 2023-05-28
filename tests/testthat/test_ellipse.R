5297cb668cac7ae53d70d3fc27bd47d52b3418e2
Charlotte Dawson <csdaw@outlook.com>
2023-05-28 12:07:10 +0100

    Simplify ellipse test for now

--- tests/testthat/test_ellipse.R
+++ tests/testthat/test_ellipse.R
@@ -2,10 +2,4 @@ test_that("ellipse default creates unit circle", {
   unit_circle <- ellipse()
 
   expect_equal(dim(unit_circle), c(360, 2))
-
-  # Just check 4 intercepts
-  expect_equal(which.max(unit_circle[, "x"]), 1)
-  expect_equal(which.min(unit_circle[, "x"]), 180)
-  expect_equal(which.max(unit_circle[, "y"]), 91)
-  expect_equal(which.min(unit_circle[, "y"]), 270)
 })
