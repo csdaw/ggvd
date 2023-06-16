8c87b8794b211e62080cde56fce7d1cbc46d050a
Charlotte Dawson <csdaw@outlook.com>
2023-06-16 18:11:16 +0100

    Add poly_clip examples

--- R/poly_clip.R
+++ R/poly_clip.R
@@ -7,13 +7,14 @@
 #'   Euclidean plane bounded by closed polygons. The format of these arguments
 #'   is either:
 #'
-#'   - a `list` containing two numeric vectors `x` and `y` giving the
-#'   coordinates of the vertices of a single polygon. The last vertex
-#'   **should not** repeat the first vertex.
-#'   - a `list of list(x,y)` structures giving the coordinates of the vertices
-#'   of several polygons.
+#'.  - a `data.frame` containing two columns `x` and `y` or a `list` containing
+#'.  two numeric vectors `x` and `y` giving the coordinates of the vertices of a
+#'   single polygon. The last vertex **should not** repeat the first vertex.
+#'   - a `list of lists(x,y)` where `x` and `y` are numeric vectors giving the
+#'   coordinates of the vertices of several polygons.
 #'
-#' @param a,b `list` or `list of list`, specifying polygons. See details.
+#' @param a,b `data.frame` or `list` or `list of lists`, specifying polygons.
+#'   See details below.
 #' @param op `string`, set operation to be performed to combine `a` and `b`.
 #'   One of `"intersection"`, `"union"`, `"minus"`, `"xor"` (partially matched).
 #'
@@ -24,7 +25,56 @@
 #' @export
 #'
 #' @examples
-#' #
+#' library(ggplot2)
+#'
+#' triangle <- list(
+#'   x = c(0, 1, 3),
+#'   y = c(0, 1, 0)
+#' )
+#'
+#' circle <- ellipse()
+#'
+#' ggplot(circle, aes(x = x, y = y)) +
+#'   geom_polygon(colour = "red") +
+#'   geom_polygon(data = data.frame(triangle), colour = "blue") +
+#'   coord_cartesian(xlim = c(-1.5, 3.5), ylim = c(-1.5, 1.5)) +
+#'   theme_bw()
+#'
+#' # 4 set operations:
+#' # intersection
+#' s1 <- poly_clip(triangle, circle, "intersection")
+#'
+#' ggplot(data.frame(s1), aes(x = x, y = y)) +
+#'   geom_polygon(colour = "green") +
+#'   coord_cartesian(xlim = c(-1.5, 3.5), ylim = c(-1.5, 1.5)) +
+#'   theme_bw()
+#'
+#' # union
+#' s2 <- poly_clip(triangle, circle, "union")
+#'
+#' ggplot(data.frame(s2), aes(x = x, y = y)) +
+#'   geom_polygon(colour = "green") +
+#'   coord_cartesian(xlim = c(-1.5, 3.5), ylim = c(-1.5, 1.5)) +
+#'   theme_bw()
+#'
+#'
+#' # minus (subtract circle from triangle)
+#' s3 <- poly_clip(triangle, circle, "minus")
+#'
+#' ggplot(data.frame(s3), aes(x = x, y = y)) +
+#'   geom_polygon(colour = "green") +
+#'   coord_cartesian(xlim = c(-1.5, 3.5), ylim = c(-1.5, 1.5)) +
+#'   theme_bw()
+#'
+#' # xor (result is 2 shapes here)
+#' s4 <- poly_clip(triangle, circle, "xor")
+#'
+#' ggplot(data.frame(s4[[1]]), aes(x = x, y = y)) +
+#'   geom_polygon(colour = "red") +
+#'   geom_polygon(data = data.frame(s4[[2]]), colour = "blue") +
+#'   coord_cartesian(xlim = c(-1.5, 3.5), ylim = c(-1.5, 1.5)) +
+#'   theme_bw()
+#'
 poly_clip <- function(a, b, op = c("intersection", "union", "minus", "xor")) {
   op <- match.arg(op)
   a0 <- identical(length(a), 0L)
